77c6c1261cd3b51c2a93b156f0eb8b0139e5c665
John Lee <leejohn24@berkeley.edu>
2020-09-25 07:32:32 -0700

    game.py update

---
+++ python-exercise/game.py
@@ -0,0 +1,22 @@
+# Tic tac toe python game  practice
+
+# @authors: Name1, Name2, ...
+
+if __name__ = "__main__":
+    tictactoe();
+
+board = {'13': ' ', '23': ' ', '33': ' ',
+        '12': ' ', '22': ' ', '32': ' ',
+        '11': ' ', '21': ' ', '31': ' ' }
+
+def main:
+    
+            
+def printer(board):
+    ## Fill in
+
+def chooseRandomMove:
+    ## Fill in
+
+def gameoverDetector:
+    ## Fill in
